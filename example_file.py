34587c855fd8cecbf75f77b4f64a87657bc0e7c7
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2020-06-24 22:52:36 +0200

    adjusted example file with 3D render in separate window, triggered by click of a button

--- example_file.py
+++ example_file.py
@@ -3,8 +3,12 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_canvas as dc
+from dash.dependencies import Input, Output, State
+import vtk_python_script as vps
+# import vtk
 
 asset_style = ['assets/style.css']
+js_script = [{'src': 'assets/testing_vtk.js', 'type': 'module'}]
 
 app = dash.Dash(__name__, external_stylesheets=asset_style)
 
@@ -12,7 +16,7 @@ app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
+        Dash: A web application framework for Python
     '''),
 
     dcc.Graph(
@@ -28,8 +32,29 @@ app.layout = html.Div(children=[
         }
     ),
     html.H5("Dash Cnavas testing"),
-    dc.DashCanvas(id='canvas-1')
+    # dc.DashCanvas(id='canvas-1')
+    html.Canvas(id="html-canvas"),
+    dcc.Loading(
+        html.Button(id="test-button")
+    )
+    
 ])
+
+@app.callback(
+    Output('test-button', 'children'),
+    [Input('test-button', 'n_clicks')]
+)
+def testing_button(clicks):
+    print("number of clicks {}".format(clicks))
+    print()
+    output  = "Start"
+    if clicks is None:
+        output = "Button"
+    else:
+        output = "Button {}".format(clicks)
+        vps.main()
+    
+    return output
 
 if __name__ == '__main__':
     app.run_server(debug=True)
