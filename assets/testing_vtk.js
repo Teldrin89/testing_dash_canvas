1c146cd1291437b417274aca0dc038122358cb45
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2020-06-29 22:28:54 +0200

    epmty (commented) JS for now

--- assets/testing_vtk.js
+++ assets/testing_vtk.js
@@ -1,74 +1,24 @@
-import 'vtk.js/Sources/favicon';
+// Load script from https://unpkg.com/vtk.js then...
+// const vtk = window.vtk
+// var fullScreenRenderer = vtk.Rendering.Misc.vtkFullScreenRenderWindow.newInstance();
+// var actor = vtk.Rendering.Core.vtkActor.newInstance();
+// var mapper = vtk.Rendering.Core.vtkMapper.newInstance();
+// var cone = vtk.Filters.Sources.vtkConeSource.newInstance();
 
-import vtkActor from 'vtk.js/Sources/Rendering/Core/Actor';
-import vtkConeSource from 'vtk.js/Sources/Filters/Sources/ConeSource';
-import vtkMapper from 'vtk.js/Sources/Rendering/Core/Mapper';
-import vtkOpenGLRenderWindow from 'vtk.js/Sources/Rendering/OpenGL/RenderWindow';
-import vtkRenderWindow from 'vtk.js/Sources/Rendering/Core/RenderWindow';
-import vtkRenderWindowInteractor from 'vtk.js/Sources/Rendering/Core/RenderWindowInteractor';
-import vtkRenderer from 'vtk.js/Sources/Rendering/Core/Renderer';
-import vtkInteractorStyleTrackballCamera from 'vtk.js/Sources/Interaction/Style/InteractorStyleTrackballCamera';
+// actor.setMapper(mapper);
+// mapper.setInputConnection(cone.getOutputPort());
 
-// ----------------------------------------------------------------------------
-// Standard rendering code setup
-// ----------------------------------------------------------------------------
+// var renderer = fullScreenRenderer.getRenderer();
+// renderer.addActor(actor);
+// renderer.resetCamera();
 
-const renderWindow = vtkRenderWindow.newInstance();
-const renderer = vtkRenderer.newInstance({ background: [0.2, 0.3, 0.4] });
-renderWindow.addRenderer(renderer);
+// var renderWindow = fullScreenRenderer.getRenderWindow();
+// renderWindow.resize(300, 400);
+// renderWindow.render();
 
-// ----------------------------------------------------------------------------
-// Simple pipeline ConeSource --> Mapper --> Actor
-// ----------------------------------------------------------------------------
-
-const coneSource = vtkConeSource.newInstance({ height: 1.0 });
-
-const mapper = vtkMapper.newInstance();
-mapper.setInputConnection(coneSource.getOutputPort());
-
-const actor = vtkActor.newInstance();
-actor.setMapper(mapper);
-
-// ----------------------------------------------------------------------------
-// Add the actor to the renderer and set the camera based on it
-// ----------------------------------------------------------------------------
-
-renderer.addActor(actor);
-renderer.resetCamera();
-
-// ----------------------------------------------------------------------------
-// Use OpenGL as the backend to view the all this
-// ----------------------------------------------------------------------------
-
-const openglRenderWindow = vtkOpenGLRenderWindow.newInstance();
-renderWindow.addView(openglRenderWindow);
-
-// ----------------------------------------------------------------------------
-// Create a div section to put this into
-// ----------------------------------------------------------------------------
-
-const container = document.createElement('div');
-document.querySelector('body').appendChild(container);
-openglRenderWindow.setContainer(container);
-
-// ----------------------------------------------------------------------------
-// Capture size of the container and set it to the renderWindow
-// ----------------------------------------------------------------------------
-
-const { width, height } = container.getBoundingClientRect();
-openglRenderWindow.setSize(width, height);
-
-// ----------------------------------------------------------------------------
-// Setup an interactor to handle mouse events
-// ----------------------------------------------------------------------------
-
-const interactor = vtkRenderWindowInteractor.newInstance();
-interactor.setView(openglRenderWindow);
-interactor.initialize();
-interactor.bindEvents(container);
-
-// ----------------------------------------------------------------------------
-// Setup interactor style to use
-// ----------------------------------------------------------------------------
-
-interactor.setInteractorStyle(vtkInteractorStyleTrackballCamera.newInstance());
+// var slider = document.querySelector('.slider');
+// slider.addEventListener('input', function (e) {
+//   var resolution = Number(e.target.value);
+//   cone.setResolution(resolution);
+//   renderWindow.render();
+// });
