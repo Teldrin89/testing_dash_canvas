5a17af863de63db6e96ef14c80f29f4e180b8750
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2020-07-01 00:22:03 +0200

    addition of if statement for slider function

--- assets/testing_vtk.js
+++ assets/testing_vtk.js
@@ -1,22 +1,29 @@
 // Load script from https://unpkg.com/vtk.js then...
-// const vtk = window.vtk
-// var fullScreenRenderer = vtk.Rendering.Misc.vtkFullScreenRenderWindow.newInstance();
-// var actor = vtk.Rendering.Core.vtkActor.newInstance();
-// var mapper = vtk.Rendering.Core.vtkMapper.newInstance();
-// var cone = vtk.Filters.Sources.vtkConeSource.newInstance();
+const vtk = window.vtk
+var fullScreenRenderer = vtk.Rendering.Misc.vtkFullScreenRenderWindow.newInstance();
+var actor = vtk.Rendering.Core.vtkActor.newInstance();
+var mapper = vtk.Rendering.Core.vtkMapper.newInstance();
+var cone = vtk.Filters.Sources.vtkConeSource.newInstance();
 
-// actor.setMapper(mapper);
-// mapper.setInputConnection(cone.getOutputPort());
+actor.setMapper(mapper);
+mapper.setInputConnection(cone.getOutputPort());
 
-// var renderer = fullScreenRenderer.getRenderer();
-// renderer.addActor(actor);
-// renderer.resetCamera();
+var renderer = fullScreenRenderer.getRenderer();
+renderer.addActor(actor);
+renderer.resetCamera();
 
-// var renderWindow = fullScreenRenderer.getRenderWindow();
+var renderWindow = fullScreenRenderer.getRenderWindow();
 // renderWindow.resize(300, 400);
-// renderWindow.render();
+renderWindow.render();
 
-// var slider = document.querySelector('.slider');
+var slider = document.querySelector('.slider');
+if (slider) {
+    slider.addEventListener('input', function (e) {
+        var resolution = Number(e.target.value);
+        cone.setResolution(resolution);
+        renderWindow.render();
+      });
+}
 // slider.addEventListener('input', function (e) {
 //   var resolution = Number(e.target.value);
 //   cone.setResolution(resolution);
