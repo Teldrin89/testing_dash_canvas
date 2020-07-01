d98150db759ac3691575aec7562ec76f5887d7ca
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2020-07-01 16:31:14 +0200

    working version of vtp file read/render with python script

--- vtk_python_script.py
+++ vtk_python_script.py
@@ -8,9 +8,20 @@ def main():
     sphereSource.SetCenter(0.0, 0.0, 0.0)
     sphereSource.SetRadius(5)
 
+    # Read poly data from vtp file
+    reader = vtk.vtkXMLPolyDataReader()
+    reader.SetFileName("data/diskout.vtp")
+    reader.Update()
+    poly_data = reader.GetOutput()
+    print("check for poly data read")
+    print(type(poly_data))
+
+
     # Create a mapper and actor
     mapper = vtk.vtkPolyDataMapper()
-    mapper.SetInputConnection(sphereSource.GetOutputPort())
+    # mapper.SetInputConnection(sphereSource.GetOutputPort())
+    # Changed mapper to read data from loaded vtp file
+    mapper.SetInputData(poly_data)
     actor = vtk.vtkActor()
     actor.SetMapper(mapper)
     actor.GetProperty().SetColor(colors.GetColor3d('Bisque'))
@@ -26,7 +37,7 @@ def main():
 
     # Add the actor to the scene
     renderer.AddActor(actor)
-    renderer.SetBackground(colors.GetColor3d('Navy'))
+    renderer.SetBackground(colors.GetColor3d('Black'))
 
     # Render and interact
     renderWindow.Render()
